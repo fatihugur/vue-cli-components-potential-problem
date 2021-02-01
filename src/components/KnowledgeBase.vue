9bee8f21f1085e5f5a56e8105d26d9460e534311
Fatih Ugur <fatihugur@gmail.com>
2021-02-01 18:33:34 +0100

    selectTopic function and event added with inject method

--- src/components/KnowledgeBase.vue
+++ src/components/KnowledgeBase.vue
@@ -1,15 +1,10 @@
 <template>
   <section>
     <h2>Select a Topic</h2>
-    <knowledge-grid
-      :topics="topics"
-      @select-topic="$emit('select-topic', $event)"
-    ></knowledge-grid>
+    <knowledge-grid></knowledge-grid>
   </section>
 </template>
 
 <script>
-export default {
-  emits: ['select-topic']
-};
+export default {};
 </script>
